f06728719330521b580df12fe31dfc903e1b599f
rochyzzle <9140305@esmad.ipp.pt>
2019-07-11 21:41:23 +0100

    adds routing system

--- eventsManagerFrontend/src/app/app.component.ts
+++ eventsManagerFrontend/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'eventsManagerFrontend';
+  public page: string = 'initial';
+
+  changePage(page: string) {
+    this.page = page;
+  }
 }
